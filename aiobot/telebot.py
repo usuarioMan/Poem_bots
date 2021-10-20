66faec16f2da2a91c24f51bf2c73ac4ad87334c2
usuarioman <castilla.ar@gmail.com>
2021-10-19 21:48:07 -0500

    Se crea un bot de telegram capaz de soportar un paradigma asincrónico.

--- aiobot/telebot.py
+++ aiobot/telebot.py
@@ -2,6 +2,10 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 from os import environ
 
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+
+from twitter.main import get_random_tweet_from_user
+
 API_TOKEN = environ['TELBOT_TOKEN']
 
 # Configure logging
@@ -17,14 +21,18 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    await message.answer(f'Hola soy el perrito poeta. Para un poema, escribe:  /p')
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
-    await message.answer(message.text)
+@dp.message_handler(commands=['p'])
+async def dame_poema(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    tweet = await get_random_tweet_from_user('MicroPoesia')
+    await message.answer(tweet)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+    "🥳"
